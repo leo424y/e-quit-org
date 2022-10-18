2777ad1bc9306828653c4df9cec3a3d5e1d0d090
Fly Chang <leo424y@gmail.com>
2022-10-18 10:03:44 +0800

    ADD: pure text

---
+++ pharse.rb
@@ -0,0 +1,19 @@
+require 'rails'
+require 'open-uri'
+require 'nokogiri'
+require 'action_view'
+
+(2357..7011).each do |id|
+  file = File.open("pages/#{id}.html")
+  file_data = file.read
+  doc = Nokogiri::HTML(file_data)
+  title = doc.xpath('//table/tr[3]/td')[0].to_s
+  # pub_date = doc.xpath('//table/tr[1]/td')[0].to_s
+  # du_date = doc.xpath('//table/tr[2]/td')[0].to_s
+  body = doc.xpath('//table/tr[4]/td')[0].to_s
+
+  title = ActionView::Base.full_sanitizer.sanitize(title).squish
+  body = ActionView::Base.full_sanitizer.sanitize(body).squish.delete(' ')
+  result = "#{title}\n#{body}"
+  File.open("./txt/#{id}", 'w+') { |file| file.write(result) } if result.present?
+end
